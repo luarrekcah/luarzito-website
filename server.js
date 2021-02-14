a834c01b0ea0d6e4cce2b4568f3ae65b40d4ff42
Glitch (hello-express) <none>
2021-02-14 06:04:14 +0000

    🍎🚕 Checkpoint ./views/comandos.html:21151839/14364 ./views/emojis.html:21151839/8068 ./views/servers.html:21151839/3044 ./views/team.html:21151839/7400 ./server.js:21151839/338 ./views/index.html:21151839/19

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/comandos", (request, response) => {
+  response.sendFile(__dirname + "/views/team.html");
+});
+
+app.get("/team", (request, response) => {
+  response.sendFile(__dirname + "/views/team.html");
+});
+
+app.get("/team", (request, response) => {
+  response.sendFile(__dirname + "/views/team.html");
+});
+
 const listener = app.listen(process.env.PORT, () => {
   console.log("Porta " + listener.address().port);
 });
