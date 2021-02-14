41ee6dc28422c41e440046553251bbdd0349826d
Glitch (hello-express) <none>
2021-02-14 15:16:26 +0000

    🎪🚶 Checkpoint ./server.js:21151839/9414 ./views/index.html:21151839/903

--- server.js
+++ server.js
@@ -1,56 +1,17 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const FormData = require("form-data");
+
+const axios = require("axios");
+const process = require("process");
+app.use(bodyParser.text());
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
- const data = new FormData();
-  console.log(req.body);
-data.append("client_id", "743841329334845530");
-  data.append("client_secret", "k7gj1sbpgm_IAV9TIxU82poDKUqifyDo");
-  data.append("grant_type", "authorization_code");
-  data.append(
-    "redirect_uri",
-    "https://luarzito.glitch.me/dashboard"
-  );
-  data.append("scope", "identify");
-  data.append("code", req.body);
-
-  fetch("https://discordapp.com/api/oauth2/token", {
-    method: "POST",
-    body: data,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const config = {
-        headers: {
-          authorization: `Bearer ${data.access_token}`,
-        },
-      };
-      axios
-        .get("https://discordapp.com/api/users/@me", config)
-        .then((response) => {
-          console.log(response.data.username);
-          res.send(response.data.username);
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-        axios
-        .get("https://discordapp.com/api/users/@me/guilds", config)
-        .then((response) => {
-         // console.log(response.data.username);
-         // res.send(response.data.username);
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    });
-    
 });
 // PC
 app.get("/comandos", (request, response) => {
@@ -73,6 +34,50 @@ app.get("/servidores", (request, response) => {
 app.get("/dashboard", (request, response) => {
   response.sendFile(__dirname + "/views/dashboard.html");
 });
+app.post("/dashboard", (req, res) => {
+  const data = new FormData();
+  console.log(req.body);
+  data.append("client_id", "743841329334845530");
+  data.append("client_secret", "k7gj1sbpgm_IAV9TIxU82poDKUqifyDo");
+  data.append("grant_type", "authorization_code");
+  data.append("redirect_uri", "https://luarzito.glitch.me/dashboard");
+  data.append("scope", "identify");
+  data.append("code", req.body);
+
+  fetch("https://discordapp.com/api/oauth2/token", {
+    method: "POST",
+    body: data
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const config = {
+        headers: {
+          authorization: `Bearer ${data.access_token}`
+        }
+      };
+      axios
+        .get("https://discordapp.com/api/users/@me", config)
+        .then(response => {
+          console.log(response.data.username);
+          res.send(response.data.username);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      axios
+        .get("https://discordapp.com/api/users/@me/guilds", config)
+        .then(response => {
+          // console.log(response.data.username);
+          // res.send(response.data.username);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
+});
 
 // MOBILE
 
@@ -92,10 +97,10 @@ const bot = new Discord.Client();
 const { Util } = require("discord.js");
 const fs = require("fs");
 const firebase = require("firebase");
-const prefixo = "dg."
+const prefixo = "dg.";
 
 bot.on("ready", () => {
-  console.log("Bot preparado.\nLogado como: " + bot.user.username)
+  console.log("Bot preparado.\nLogado como: " + bot.user.username);
 });
 
 try {
