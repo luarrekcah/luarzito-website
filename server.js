da53eb8479066ddb68e4e7560ad4783dca9b076a
Glitch (hello-express) <none>
2021-02-14 13:54:36 +0000

    🗽🛰 Checkpoint ./package.json:21151839/1432 ./server.js:21151839/3493 ./comandos/infos/ajuda.js:21151839/1955 ./eventos/message.js:21151839/20596

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.use(express.static("public"));
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
+  response.sendStatus(200);
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(
+    `Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
+  );
 });
 // PC
 app.get("/comandos", (request, response) => {
@@ -37,132 +43,18 @@ app.get("/mobile/comandos", (request, response) => {
 const Discord = require("discord.js");
 const http = require("http");
 const moment = require("moment");
-
 const bot = new Discord.Client();
 const { Util } = require("discord.js");
 const fs = require("fs");
 const firebase = require("firebase");
 
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-  const ping = new Date();
-  ping.setHours(ping.getHours() - 3);
-  console.log(
-    `Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
-  );
-});
-app.listen(process.env.PORT);
-
-bot.Discord = Discord;
-bot.util = Util;
-bot.fs = fs;
-
-bot.github = "https://github.com/luarrekcah/";
-bot.webpage = "https://luarzito.glith.me/";
-bot.inviteLink = "https://discordapp.com/oauth2/authorize?client_id=743841329334845530&scope=bot&permissions=2080697599";
-bot.dev = ["701953428510736396"];
-bot.prefixo = "lz.";
-
-bot.emojis = {
-  alerta: "<a:alerta:758339902386733098>"
-};
-
-var configF = {
-  apiKey: process.env.FB_apiKey,
-  authDomain: process.env.FB_authDomain,
-  databaseURL: process.env.FB_databaseURL,
-  projectId: process.env.FB_projectID,
-  storageBucket: process.env.FB_storageBucket,
-  messagingSenderId: process.env.FB_messagingSenderId,
-  appId: process.env.FB_appId
-};
-
-firebase.initializeApp(configF);
-
-const database = firebase.database();
-
-bot.pastas = {};
-bot.listas_comandos = {};
-var logcrr = "Carreguei módulos: ";
-var diretórios = ["comandos"];
-var pasta, subpasta;
-while (diretórios.length) {
-  pasta = diretórios.shift();
-  try {
-    let nomes_arquivos = bot.fs.readdirSync("./" + pasta + "/");
-    for (let nomea of nomes_arquivos) {
-      if (!nomea.endsWith(".js")) {
-        diretórios.unshift(pasta + "/" + nomea);
-        continue;
-      }
-      try {
-        let código = require("./" + pasta + "/" + nomea);
-        nomea = nomea.split(".")[0];
-        if (bot[nomea]) console.log("Existe 2 .js de mesmo nome: " + nomea);
-
-        bot[nomea] = código.run;
-        bot.pastas[nomea] = pasta;
-        logcrr += nomea + ", ";
-
-        if (pasta.includes("comandos/")) {
-          subpasta = pasta.slice(9);
-          if (!bot.listas_comandos[subpasta])
-            bot.listas_comandos[subpasta] = [];
-
-          bot.listas_comandos[subpasta].push(bot.prefixo + nomea + "\n");
-        }
-      } catch (erro) {
-        console.log(
-          "##Erro ao carregar " +
-            nomea +
-            ": " +
-            erro.message +
-            " - " +
-            erro.name
-        );
-      }
-    }
-  } catch (erro) {
-    console.log(
-      "##Erro ao ler " + pasta + ": " + erro.message,
-      " - ",
-      erro.name
-    );
-  }
-}
-console.log(logcrr + "carregados.");
-
-bot.nome_eventos = [];
-var logcrr = "Carreguei eventos: ";
-try {
-  let arquivos_eventos = bot.fs.readdirSync("./eventos/");
-  for (let nomee of arquivos_eventos) {
-    if (!nomee.endsWith(".js")) continue;
-    try {
-      let código = require("./eventos/" + nomee);
-      nomee = nomee.split(".")[0]; //remove .js
-      bot.on(nomee, código.bind(null, bot));
-      logcrr += nomee + ", ";
-      bot.nome_eventos.push(nomee);
-    } catch (erro) {
-      console.log(
-        "##Erro ao carregar o evento " +
-          nomee +
-          ": " +
-          erro.message +
-          " - " +
-          erro.name
-      );
-    }
-  }
-} catch (erro) {
-  console.log("##Erro ao ler eventos! " + erro.message + " - " + erro.name);
-}
-console.log(logcrr + "carregados.");
+bot.on("ready", () => {
+  console.log("Bot preparado.\nLogado como: " + bot.user.username)
+})
 
 try {
   console.log("Tentando logar na api do dc");
-  bot.login(process.env.TOKEN);
+  bot.login(process.env.tokenBot);
 } catch (e) {
   console.log("Erro ao logar na api do dc: ", e);
 }
