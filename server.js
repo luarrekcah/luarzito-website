8940ad68f61ac8a677092c36e1323abe2381fc00
Glitch (hello-express) <none>
2021-02-14 06:15:28 +0000

    😽👫 Checkpoint ./server.js:21151839/4982 ./views/m/index.html:21151839/19658 ./views/m/comandos.html:21151839/24060 ./views/index.html:21151839/4654

--- server.js
+++ server.js
@@ -7,17 +7,31 @@ app.use(express.static("public"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
-
+// PC
 app.get("/comandos", (request, response) => {
-  response.sendFile(__dirname + "/views/team.html");
+  response.sendFile(__dirname + "/views/comandos.html");
+});
+
+app.get("/emojis", (request, response) => {
+  response.sendFile(__dirname + "/views/emojis.html");
 });
 
 app.get("/team", (request, response) => {
   response.sendFile(__dirname + "/views/team.html");
 });
 
-app.get("/team", (request, response) => {
-  response.sendFile(__dirname + "/views/team.html");
+app.get("/servidores", (request, response) => {
+  response.sendFile(__dirname + "/views/servers.html");
+});
+
+// MOBILE 
+
+app.get("/mobile", (request, response) => {
+  response.sendFile(__dirname + "/views/m/index.html");
+});
+
+app.get("/mobile/comandos", (request, response) => {
+  response.sendFile(__dirname + "/views/m/comandos.html");
 });
 
 const listener = app.listen(process.env.PORT, () => {
