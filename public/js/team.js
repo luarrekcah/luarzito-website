9b82ac397962c4ce930294e2be238ce1eb958971
Glitch (hello-express) <none>
2021-02-18 14:10:17 +0000

    📚🌛 Checkpoint ./public/js/team.js:21151839/5124 ./views/team.html:21151839/7062

--- public/js/team.js
+++ public/js/team.js
@@ -28,9 +28,24 @@ function card(avatar, nome, cargo, redeSocialLink, redeSocialIcone) {
 }
 
 const p1 = { //P1 === Raul / Luar Rekcah
-  avatar: "",
-  nome: "",
-  cargo: "",
+  avatar: "https://cdn.discordapp.com/avatars/701953428510736396/ec3f48a1870024894c42825d8eee0e97.png?size=1024",
+  nome: "Raul ᵈᵃ ᵇⁱᵃʰ ♡⃤ 🦋⃤#5811",
+  cargo: "CEO | Desenvolvedor",
+  redeSocialLink: "",
+  redeSocialIcone: ""
+};
+const p2 = { //P2 === Bia
+  avatar: "https://cdn.discordapp.com/avatars/666382842338607134/717e3b944e95a8e20f03989e358a01c5.png?size=1024",
+  nome: "Biah ᵈᵒ ʳᵃᵘˡ ♡⃤ 🦋⃤#1112",
+  cargo: "CPO",
+  redeSocialLink: "",
+  redeSocialIcone: ""
+};
+
+const p3 = { //P3 === Angel
+  avatar: "https://cdn.discordapp.com/avatars/740298343783202865/af5ed8f7b92b5ce487d05048953925b1.png?size=1024",
+  nome: "Angel ツ#3912",
+  cargo: "COO",
   redeSocialLink: "",
   redeSocialIcone: ""
 };
@@ -38,3 +53,7 @@ const p1 = { //P1 === Raul / Luar Rekcah
 //Cards
 
 card(p1.avatar, p1.nome, p1.cargo, p1.redeSocialLink, p1.redeSocialIcone);
+
+card(p2.avatar, p2.nome, p2.cargo, p2.redeSocialLink, p2.redeSocialIcone);
+
+card(p3.avatar, p3.nome, p3.cargo, p3.redeSocialLink, p3.redeSocialIcone);
