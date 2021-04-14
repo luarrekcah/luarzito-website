3e8d435b2cd65a02690443739118e156d0af8fc5
Glitch (luarzito) <none>
2021-04-14 18:24:21 +0000

    🚛👰 Checkpoint ./public/js/src/comandos/rp.js:21151839/542 ./views/comandos.html:21151839/839 ./public/js/src/comandos/social.js:21151839/1448

--- public/js/src/comandos/rp.js
+++ public/js/src/comandos/rp.js
@@ -1,8 +1,19 @@
-const comandosRp = [
-  ["", ""],
-  ["", ""],
-  ["", ""],
+//Finalizado
 
+const comandosRp = [
+  ["esfaquear", "Dê uma facada em alguém, ele terá um tempo para reagir!"],
+  ["acordar", "Aaaa, acordaaaaa (-_- )"],
+  ["bater", "Cê está merecendo uns tapas!"],
+  ["cafune", "Ownn, dengo."],
+  ["f", "F to pray respect."],
+  ["handshake", "Aperto de mão, brow."],
+  ["hug", "Abraçooo!"],
+  ["kiss", "Beijinho :D"],
+  ["piscar", "Piscadinha para você!"],
+  ["poke", "Ok, me dê atenção!"],
+  ["putsleep", "Já deu hora para dormir, rum!"],
+  ["smile", "Sorrisinhoo!"],
+  ["tchau ", "Até logo!"]
 ]
 
 function comando(cmd, desc) {
--- public/js/src/comandos/social.js
+++ public/js/src/comandos/social.js
@@ -1,14 +1,16 @@
 const comandosSo = [
-  ["", ""],
-  ["", ""],
-  ["", ""],
-
-]
+  ["casar", "Se case com sua alma gêmea!"],
+  ["perfil", "Veja seu perfil!"],
+  [
+    "rep",
+    "Dê pontos de reputação para alguém que lhe ajudou, ou fez algo legal."
+  ],
+  ["bio", `Atualize seu "sobre mim".`]
+];
 
 function comando(cmd, desc) {
   document.write(`<p><b>${cmd}</b> - ${desc}</p>`);
 }
-
 let iSo = 0;
 
 for (iSo = 0; iSo < comandosSo.length; iSo++) {
