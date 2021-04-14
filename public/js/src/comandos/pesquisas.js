e9942764863df4d0f564ab6b4716a0fae71867b7
Glitch (luarzito) <none>
2021-04-14 15:08:01 +0000

    💸🍙 Checkpoint ./public/js/src/comandos/pesquisas.js:21151839/1838

--- public/js/src/comandos/pesquisas.js
+++ public/js/src/comandos/pesquisas.js
@@ -1,9 +1,14 @@
 const comandos = [
   ["book ", "Pesquise e ache Informações sobre livros."],
-  ["", ""],
-  ["", ""],
-
-]
+  ["clima", "Veja o clima da sua cidade."],
+  ["image", "Pesquise imagens, isso é muito útil."],
+  ["ipget", "Puxe dados de um IP pelo Discord!"],
+  ["ly", "Pesquise as letras de uma música!"],
+  ["wiki", "Faça pesquisas no Wikipedia."],
+  ["yt", "Pesquise vídeos no YouTube."],
+  ["cursed", "Bizarro..."],
+  ["mc", "Informações sobre os usuários de Minecraft e derivados!"]
+];
 
 function comando(cmd, desc) {
   document.write(`<p><b>${cmd}</b> - ${desc}</p>`);
@@ -14,9 +19,3 @@ let i = 0;
 for (i = 0; i < comandos.length; i++) {
   comando(comandos[i][0], comandos[i][1]);
 }
-
-
-/*
-* COLOCAR NO PESQUISAS:
-* -- mc
-*/
