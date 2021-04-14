e1217728b152716b826261bff93238c210195af6
Glitch (luarzito) <none>
2021-04-14 13:58:49 +0000

    🌝🍈 Checkpoint ./public/js/src/comandos/admin.js:21151839/1935 ./views/comandos.html:21151839/212

--- public/js/src/comandos/admin.js
+++ public/js/src/comandos/admin.js
@@ -1,19 +1,11 @@
-function comando(cmd, desc) {
-  document.write(`<p><b>${cm}</b> - Adicione emoji apartir de um link!</p>`)
-} 
+const comandos = ["teste", "teste2"]
 
-<p><b>addemoji</b> - Adicione emoji apartir de um link!</p>
-        <p><b>ban</b> - Bana um usuário indesejado.</p>
-        <p><b>ch</b> - Ferramentas para gestão do canal.</p>
-        <p>
-          <b>clear</b> - Limpe as mensagens do chat, escolha de 1 à 99 ou 200!
-        </p>
-        <p><b>kick</b> - Chute um usuário, ele ainda poderá retornar...</p>
-        <p><b>listban</b> - Receba a lista de bans na DM.</p>
-        <p><b style="color: red;">mute</b> - Em breve...</p>
-        <p><b>say</b> - O que acha de me fazer falar umas coisas? Tente!</p>
-        <p><b>sv</b> - Ferramentas para gestão do servidor.</p>
-        <p>
-          <b>unban</b> - Dê uma segunda chance à aquele renegado... Desbana pelo
-          ID.
-        </p>
+function comando(cmd, desc) {
+  document.write(`<p><b>${cmd}</b> - ${desc}</p>`);
+}
+
+let i = 0;
+
+for (i = 0; i < comandos.length; i++) {
+  comando(comandos[i], "desc");
+}
