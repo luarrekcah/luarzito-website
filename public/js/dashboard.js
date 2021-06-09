8b0730a39ad5c1cabfa22cebde5dea58e4ad6cfd
Glitch (luarzito) <none>
2021-06-09 03:16:20 +0000

    🐇🐑 Checkpoint ./public/js/dashboard.js:21151839/2136 ./views/dashboard.html:21151839/4490

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,3 @@
-
 window.onload = () => {
   const code = location.href.substring(
     location.href.indexOf("code") + 5,
@@ -10,16 +9,17 @@ window.onload = () => {
 
     req.open("POST", "https://luarzito.glitch.me/dashboard");
     req.send(code);
-    
-    
+
     req.onload = () => {
-      
-    var perfil = JSON.parse(req.responseText);
-    document.getElementById("login-btn").style.display = "none";
-      document.getElementeById("login").style.display = "none"
-    console.log(perfil);
+      var perfil = JSON.parse(req.responseText);
+      const link = `https://cdn.discordapp.com/avatars/${perfil.id}/${perfil.avatar}.png?size=1024`;
+      document.getElementById("perfil-foto").src = link;
+      document.getElementById("perfil-nome").innerHTML = perfil.username + "#" + perfil.discriminator; 
+      document.getElementById("login-btn").style.display = "none";
+      document.getElementById("login").style.display = "none";
+      document.getElementById("header-login").style.display = "block";
+      console.log(perfil);
       document.getElementById("display_result").innerText = req.response;
     };
-  
   }
 };
