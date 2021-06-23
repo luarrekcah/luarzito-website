9cd8a97ac79940cc2b3ffa2109aef1c5c85c804e
Glitch (luarzito) <none>
2021-06-23 21:00:32 +0000

    ❤️🏥 Checkpoint ./public/js/dashboard.js:21151839/24278

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -16,126 +16,245 @@ let link, perfil;
 //---------------------------------------------------------------------
 
 window.onload = () => {
-  const code = location.href.substring(
-    location.href.indexOf("code") + 5,
-    location.href.length
-  );
+  console.log(localStorage.getItem("login"))
+  if (localStorage.getItem("login") != undefined) {
+    
+    const req = JSON.parse(localStorage.getItem("login"))
+        //SETANDO VARIAVEIS UNDEFINED
 
-  if (location.href.indexOf("code") > -1) {
-    const req = new XMLHttpRequest();
+        console.log(req);
+        localStorage.setItem("login", req.responseText);
+        perfil = JSON.parse(req.responseText);
+        link = `https://cdn.discordapp.com/avatars/${perfil.id}/${perfil.avatar}.png?size=1024`;
 
-    req.open("POST", "https://luarzito.glitch.me/dashboard");
-    req.send(code);
-
-    req.onload = () => {
-      //SETANDO VARIAVEIS UNDEFINED
-      console.log(req);
-      perfil = JSON.parse(req.responseText);
-      link = `https://cdn.discordapp.com/avatars/${perfil.id}/${perfil.avatar}.png?size=1024`;
-
-      //---------------------------------------------------------------------
-
-      //LIBERADOR ONLOGIN
-      let i;
-      for (i = 0; i < escolhas.length; i++) {
-        console.log(i);
-        escolhas[i].style.cursor = "default";
-      }
-      //---------------------------------------------------------------------
-
-      //ONLOGIN
-      avatar.src = link;
-      username.innerText = `Olá ${perfil.username}!`;
-      //document.getElementById("display_result").innerText = req.response;
-      document.getElementById("me_add").style.display = "none";
-
-      //---------------------------------------------------------------------
-
-      let perfisDB = firebase.database().ref(`Perfis/${perfil.id}`);
-      perfisDB.once("value", db => {
-        //VALORES INICIAIS
-        ls.innerText = `Lennes: ${db.val().Reais}`;
-        reps.innerText = `REPs: ${db.val().Reps}`;
-        aboutInput.value = `${db.val().sobremim}`;
-        onLogin.style.display = "block";
-        capa.style.backgroundImage = "url(" + db.val().fundo_perfil + ")";
         //---------------------------------------------------------------------
 
-        //BOTAO 1
-        aboutButton.addEventListener("click", function() {
-          if (aboutInput.value != db.val().sobremim) {
+        //LIBERADOR ONLOGIN
+        let i;
+        for (i = 0; i < escolhas.length; i++) {
+          console.log(i);
+          escolhas[i].style.cursor = "default";
+        }
+        //---------------------------------------------------------------------
+
+        //ONLOGIN
+        avatar.src = link;
+        username.innerText = `Olá ${perfil.username}!`;
+        //document.getElementById("display_result").innerText = req.response;
+        document.getElementById("me_add").style.display = "none";
+
+        //---------------------------------------------------------------------
+
+        let perfisDB = firebase.database().ref(`Perfis/${perfil.id}`);
+        perfisDB.once("value", db => {
+          //VALORES INICIAIS
+          ls.innerText = `Lennes: ${db.val().Reais}`;
+          reps.innerText = `REPs: ${db.val().Reps}`;
+          aboutInput.value = `${db.val().sobremim}`;
+          onLogin.style.display = "block";
+          capa.style.backgroundImage = "url(" + db.val().fundo_perfil + ")";
+          //---------------------------------------------------------------------
+
+          //BOTAO 1
+          aboutButton.addEventListener("click", function() {
+            if (aboutInput.value != db.val().sobremim) {
+              perfisDB
+                .update({
+                  sobremim: aboutInput.value
+                })
+                .then(alert("Sua bio foi atualizada."));
+            } else {
+              return;
+            }
+          });
+          //---------------------------------------------------------------------
+
+          //BOTAO 2
+          deletar.addEventListener("click", function() {
+            let confirmacao = confirm("Deseja apagar os dados?");
+            if (confirmacao) {
+              perfisDB
+                .child(db.key)
+                .remove()
+                .catch(console.log);
+
+              /*  location.reload();
+        return false;*/
+            }
+          });
+
+          //---------------------------------------------------------------------
+
+          //BOTAO 3
+          reset.addEventListener("click", function() {
+            let confirmacao = confirm("Deseja resetar os dados?");
+            if (confirmacao) {
+              perfisDB
+                .set({
+                  Reps: 0,
+                  Reps_autores: [],
+                  Reps_razoes: [],
+                  Reais: 0,
+                  fundo_perfil:
+                    "https://cdn.discordapp.com/attachments/742068003583295623/792531715863609354/Bgnzinho_Img027.png",
+                  casadocom_tempo: 0,
+                  casadocom: 0,
+                  sobremim: ""
+                })
+                .then(() => {
+                  location.reload();
+                  return false;
+                });
+            }
+          });
+
+          //---------------------------------------------------------------------
+
+          //BOTAO 4
+          capa.addEventListener("click", function() {
+            let link = prompt("Cole o link da sua nova imagem de perfil:");
+            if (!link.startsWith("http"))
+              return alert("Desculpe, mas isto não é um link válido. :c");
             perfisDB
               .update({
-                sobremim: aboutInput.value
-              })
-              .then(alert("Sua bio foi atualizada."));
-          } else {
-            return;
-          }
-        });
-        //---------------------------------------------------------------------
-
-        //BOTAO 2
-        deletar.addEventListener("click", function() {
-          let confirmacao = confirm("Deseja apagar os dados?");
-          if (confirmacao) {
-            perfisDB
-              .child(db.key)
-              .remove()
-              .catch(console.log);
-
-            /*  location.reload();
-        return false;*/
-          }
-        });
-
-        //---------------------------------------------------------------------
-
-        //BOTAO 3
-        reset.addEventListener("click", function() {
-          let confirmacao = confirm("Deseja resetar os dados?");
-          if (confirmacao) {
-            perfisDB
-              .set({
-                Reps: 0,
-                Reps_autores: [],
-                Reps_razoes: [],
-                Reais: 0,
-                fundo_perfil:
-                  "https://cdn.discordapp.com/attachments/742068003583295623/792531715863609354/Bgnzinho_Img027.png",
-                casadocom_tempo: 0,
-                casadocom: 0,
-                sobremim: ""
+                fundo_perfil: link
               })
               .then(() => {
-                location.reload();
-                return false;
+                alert("Sua foto foi atualizada.");
+                capa.style.backgroundImage = "url(" + link + ")";
+              })
+              .catch(erro => {
+                alert(erro);
               });
-          }
+          });
+
+          //---------------------------------------------------------------------
         });
+     
+  } else {
+    const code = location.href.substring(
+      location.href.indexOf("code") + 5,
+      location.href.length
+    );
+
+    if (location.href.indexOf("code") > -1) {
+      const req = new XMLHttpRequest();
+
+      req.open("POST", "https://luarzito.glitch.me/dashboard");
+      req.send(code);
+
+      req.onload = () => {
+        //SETANDO VARIAVEIS UNDEFINED
+
+        console.log(req);
+        localStorage.setItem("login", req.responseText);
+        perfil = JSON.parse(req.responseText);
+        link = `https://cdn.discordapp.com/avatars/${perfil.id}/${perfil.avatar}.png?size=1024`;
 
         //---------------------------------------------------------------------
 
-        //BOTAO 4
-        capa.addEventListener("click", function() {
-          let link = prompt("Cole o link da sua nova imagem de perfil:");
-          if (!link.startsWith("http"))
-            return alert("Desculpe, mas isto não é um link válido. :c");
-          perfisDB
-            .update({
-              fundo_perfil: link
-            })
-            .then(() => {
-              alert("Sua foto foi atualizada.");
-              capa.style.backgroundImage = "url(" + link + ")";
-            })
-            .catch(erro => {
-              alert(erro);
-            });
-        });
+        //LIBERADOR ONLOGIN
+        let i;
+        for (i = 0; i < escolhas.length; i++) {
+          console.log(i);
+          escolhas[i].style.cursor = "default";
+        }
+        //---------------------------------------------------------------------
+
+        //ONLOGIN
+        avatar.src = link;
+        username.innerText = `Olá ${perfil.username}!`;
+        //document.getElementById("display_result").innerText = req.response;
+        document.getElementById("me_add").style.display = "none";
 
         //---------------------------------------------------------------------
-      });
-    };
+
+        let perfisDB = firebase.database().ref(`Perfis/${perfil.id}`);
+        perfisDB.once("value", db => {
+          //VALORES INICIAIS
+          ls.innerText = `Lennes: ${db.val().Reais}`;
+          reps.innerText = `REPs: ${db.val().Reps}`;
+          aboutInput.value = `${db.val().sobremim}`;
+          onLogin.style.display = "block";
+          capa.style.backgroundImage = "url(" + db.val().fundo_perfil + ")";
+          //---------------------------------------------------------------------
+
+          //BOTAO 1
+          aboutButton.addEventListener("click", function() {
+            if (aboutInput.value != db.val().sobremim) {
+              perfisDB
+                .update({
+                  sobremim: aboutInput.value
+                })
+                .then(alert("Sua bio foi atualizada."));
+            } else {
+              return;
+            }
+          });
+          //---------------------------------------------------------------------
+
+          //BOTAO 2
+          deletar.addEventListener("click", function() {
+            let confirmacao = confirm("Deseja apagar os dados?");
+            if (confirmacao) {
+              perfisDB
+                .child(db.key)
+                .remove()
+                .catch(console.log);
+
+              /*  location.reload();
+        return false;*/
+            }
+          });
+
+          //---------------------------------------------------------------------
+
+          //BOTAO 3
+          reset.addEventListener("click", function() {
+            let confirmacao = confirm("Deseja resetar os dados?");
+            if (confirmacao) {
+              perfisDB
+                .set({
+                  Reps: 0,
+                  Reps_autores: [],
+                  Reps_razoes: [],
+                  Reais: 0,
+                  fundo_perfil:
+                    "https://cdn.discordapp.com/attachments/742068003583295623/792531715863609354/Bgnzinho_Img027.png",
+                  casadocom_tempo: 0,
+                  casadocom: 0,
+                  sobremim: ""
+                })
+                .then(() => {
+                  location.reload();
+                  return false;
+                });
+            }
+          });
+
+          //---------------------------------------------------------------------
+
+          //BOTAO 4
+          capa.addEventListener("click", function() {
+            let link = prompt("Cole o link da sua nova imagem de perfil:");
+            if (!link.startsWith("http"))
+              return alert("Desculpe, mas isto não é um link válido. :c");
+            perfisDB
+              .update({
+                fundo_perfil: link
+              })
+              .then(() => {
+                alert("Sua foto foi atualizada.");
+                capa.style.backgroundImage = "url(" + link + ")";
+              })
+              .catch(erro => {
+                alert(erro);
+              });
+          });
+
+          //---------------------------------------------------------------------
+        });
+      };
+    }
   }
 };
